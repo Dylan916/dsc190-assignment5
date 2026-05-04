66d3ea19816338b36da6dd5575a410061bbe451b
Dylan Louie <85195096+Dylan916@users.noreply.github.com>
2026-05-04 16:04:42 -0700

    testing if the lint protection works

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from dsc190-assignment5!")
+import os
+import sys
 
+def main():
+    print("Hello from dsc190-assignment5!" )   
+    l = []
+    for i in range(10):
+        l.append(i)
+    if l == None:
+        pass
+    return l
 
 if __name__ == "__main__":
     main()
