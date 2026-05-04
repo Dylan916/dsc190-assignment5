00f34631b1a289d97f661e8fb91285b8ce9c9b76
Dylan Louie <85195096+Dylan916@users.noreply.github.com>
2026-05-04 16:09:17 -0700

    fixed the lint errors

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import os
-import sys
-
 def main():
     print("Hello from dsc190-assignment5!" )   
-    l = []
-    for i in range(10):
-        l.append(i)
-    if l == None:
-        pass
-    return l
 
 if __name__ == "__main__":
     main()
